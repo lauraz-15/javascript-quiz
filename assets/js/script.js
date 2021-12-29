bb1076657ac0e77817e8841daff5ad3bcbc7ce4f
lauraz-15 <laura.zvirbule90@gmail.com>
2021-12-29 21:30:02 +0000

    Add question div to be visible on the first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
 console.log("This is connected")
 
+// add questions and answers in an array of objects;
+
 // Load page and start the event listeners
+
+// check if user has clicked START button, if so, then start the game
+
+// function showQuestion 
+// - Pick random question out of the question array and display to the user
+
+// function startGame
+// 1. gets the question and answers and displays to the user
+// 2. update the question counter (1/30 etc)
+// 3. clear the previous score and set to 0;
+
+// function checkAnswer
+// -listen to the users choice
+// -change the background of the correct & incorrect answers
+// -compare users choice to the correct answer(if correct update score)
+
+// function nextQuestion 
+// - listen to users action to press the NEXT Q button
+// - check the question counter, if number is less than 30 then call shoqQuestion function
+// - uodate question counter ++
+
+// function dipslayGameResults
+// -didpsly massive banner, and depending on the score tex explaining the level
+// add button: "Pay again" - call startGame function
+
+
+
